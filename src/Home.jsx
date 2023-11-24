72ca2967e80451b07d62e99b9a6f0c97b501c9fd
bruninhuAl <brunoacristo13@gmail.com>
2023-11-24 17:33:10 -0400

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,37 @@
+import "./home.css"
 export default function Home(){
     return(
-        <h1>PTAC 2 - PROJETO FINAL</h1>
+        <div>
+
+<body>
+    <div class="container">
+        
+
+        <nav class="sidebar">
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a href="./destaque.jsx">Em Destaque</a></li>
+                <li><a href="./register.jsx">Novo Vídeo</a></li>
+            </ul>
+        </nav>
+
+        <main>
+            <section class="video-card">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
+                <div class="video-info">
+                    <h2>Video Title</h2>
+                    <p>Video description goes here.</p>
+                </div>
+            </section>
+
+            
+
+        </main>
+    </div>
+    
+</body>
+
+
+        </div>
      );
 }
