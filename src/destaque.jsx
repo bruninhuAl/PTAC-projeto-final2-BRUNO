543ee5431c6b66e37c99803520fb7403b9cdd233
bruninhuAl <brunoacristo13@gmail.com>
2023-12-08 17:17:48 -0400

    iframe

---
+++ src/destaque.jsx
@@ -0,0 +1,22 @@
+import { link } from "react-router-dom"
+import "..style/card.css"
+
+function Cards({ListaVideos}) {
+
+    return (
+        
+        ListaVideos.map((video) =>
+            <div className="Card">
+                <iframe
+                    width="853"
+                    height="480"
+                    src={
+                        'https://www.youtube.com/embed/' + video.url.slice(17)}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media"
+                </iframe>
+            </div>
+        
+        )
+    )
+}
