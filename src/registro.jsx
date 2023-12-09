c6a968e98c49000ba60596ce1f9b7a3ee9039cb1
Bruninhu <brunoacristo13@gmail.com>
2023-12-09 17:25:32 -0300

    commit main
    arrumando erros de rota que nao esta funcionando

--- src/registro.jsx
+++ src/registro.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Todo() {
+export default function registro() {
   const listaLocalStorage = JSON.parse(localStorage.getItem("Lista")) || [];
   const [lista, setLista] = useState(listaLocalStorage);
 
