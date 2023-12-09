94dd0d81d825d0387428164be4704cdc126568eb
Bruninhu <brunoacristo13@gmail.com>
2023-12-09 18:03:07 -0300

    commit card
    arrumando os cards ainda

--- src/card.jsx
+++ src/card.jsx
@@ -2,4 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Card.css";
 
-export default function Card({ x }) 
+export default function Card({ x }) {
+    return(
+        <div>
+            <iframe 
+                width="796" 
+                height="448"
+                src={
+                    `https://www.youtube.com/embed/` ${X}}
+                frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+            </iframe>
+        </div>
+    )
+}
